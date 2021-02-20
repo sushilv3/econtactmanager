2944453113868dbfb2e7327a1a925cb3610d3d34
sushilv3 <sushilv3rma@gmail.com>
2021-02-20 22:54:46 +0530

    h2 data base config

---
+++ src/main/java/com/econtact/EcontactmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.econtact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcontactmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcontactmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econtact/EcontactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econtact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcontactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
