a9c528737b10c95c9b6fbdbb2be1ab631118f40b
sushilv3 <sushilv3rma@gmail.com>
2021-02-25 16:56:05 +0530

    added user register handler

--- src/main/java/com/econtact/controllers/HomeController.java
+++ src/main/java/com/econtact/controllers/HomeController.java
@@ -1,28 +1,70 @@
 package com.econtact.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.econtact.entities.User;
+import com.econtact.helper.Message;
+import com.econtact.repositories.UserRepository;
 
 @Controller
 public class HomeController {
-	
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("title", "Home - E Contct Manager");
 		return "home";
 	}
-	
+
 	@RequestMapping("/about")
 	public String about(Model model) {
 		model.addAttribute("title", "Home - E Contct Manager");
 		return "about";
 	}
-	
+
 	@RequestMapping("/signup")
 	public String signup(Model model) {
 		model.addAttribute("title", "Register - E Contct Manager");
+		model.addAttribute("user", new User());
 		return "signup";
 	}
+
+	// this handler for registering
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+			HttpSession session) {
+		System.out.println("Agreement " + agreement);
+		System.out.println("User " + user);
+		try {
+			if (!agreement) {
+				System.out.println("You have not agreed the terms and conditions");
+				throw new Exception("You have not agreed the terms and conditions");
+			}
+			user.setRole("ROLE_USER");
+			user.setEnable(true);
+			User result = this.userRepository.save(user);
+
+			model.addAttribute("user", new User());
+			session.setAttribute("message", new Message("Successfully Register !!", "alert-success"));
+			return "signup";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something Went Wrong !!" + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
 	
+	}
+
 }
