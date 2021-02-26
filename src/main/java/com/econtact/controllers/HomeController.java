656cbd2e17d53a959e18b9a78d3ef812c850f7a1
sushilv3 <sushilv3rma@gmail.com>
2021-02-26 16:16:30 +0530

    added BindingResult for validating

--- src/main/java/com/econtact/controllers/HomeController.java
+++ src/main/java/com/econtact/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package com.econtact.controllers;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,7 +42,7 @@ public class HomeController {
 
 	// this handler for registering
 	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
-	public String registerUser(@ModelAttribute("user") User user,
+	public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult result1,
 			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
 			HttpSession session) {
 		System.out.println("Agreement " + agreement);
@@ -49,6 +51,11 @@ public class HomeController {
 			if (!agreement) {
 				System.out.println("You have not agreed the terms and conditions");
 				throw new Exception("You have not agreed the terms and conditions");
+			}
+			if(result1.hasErrors()) {
+				System.out.println("ERROR "+ result1.toString());
+				model.addAttribute("user", user);
+				return "signup";
 			}
 			user.setRole("ROLE_USER");
 			user.setEnable(true);
@@ -66,5 +73,6 @@ public class HomeController {
 		}
 	
 	}
+	//
 
 }
