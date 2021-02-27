22b5ef5f97e798b83c4e802276c0851d377dc76d
sushilv3 <sushilv3rma@gmail.com>
2021-02-27 17:33:56 +0530

    written jpa select query

--- src/main/java/com/econtact/repositories/UserRepository.java
+++ src/main/java/com/econtact/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.econtact.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.econtact.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+	@Query("select u from User u where u.email = :email")
+	public User GetUserByUserName(@Param("email") String email);
 
 }
