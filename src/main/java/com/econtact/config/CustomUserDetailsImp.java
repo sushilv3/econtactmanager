40fa92fca472bfb1a6b9e116f7abe751b2f5eb0b
sushilv3 <sushilv3rma@gmail.com>
2021-02-27 17:36:33 +0530

    added CustomUserDetailsImp

---
+++ src/main/java/com/econtact/config/CustomUserDetailsImp.java
@@ -0,0 +1,65 @@
+package com.econtact.config;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.econtact.entities.User;
+
+public class CustomUserDetailsImp  implements UserDetails{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private User user;
+	
+	public CustomUserDetailsImp(User user) {
+		this.user = user;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
+		return List.of(simpleGrantedAuthority);
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+
+}
