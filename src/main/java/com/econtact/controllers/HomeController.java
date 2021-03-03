f9aa4286fc64c19c9c6f8eab61e04344106f4653
sushilv3 <sushilv3rma@gmail.com>
2021-03-03 13:42:18 +0530

    added signin handler

--- src/main/java/com/econtact/controllers/HomeController.java
+++ src/main/java/com/econtact/controllers/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -79,6 +80,11 @@ public class HomeController {
 		}
 	
 	}
-	//
+	//handler for custom login
+	@GetMapping("/signin")
+	public String customLogin(Model model) {
+		model.addAttribute("title", "Login Page");
+		return "login";
+	}
 
 }
