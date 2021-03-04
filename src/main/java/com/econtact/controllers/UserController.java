285ed4b578dab947bdb90d9aa4b3f4812225bdbb
sushilv3 <sushilv3rma@gmail.com>
2021-03-04 11:53:36 +0530

    updated user index mapping

--- src/main/java/com/econtact/controllers/UserController.java
+++ src/main/java/com/econtact/controllers/UserController.java
@@ -1,16 +1,33 @@
 package com.econtact.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.econtact.entities.User;
+import com.econtact.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping("/index")
-	public String dashBoard() {
+	public String dashBoard(Model model,Principal principal) {
 		
-
+		String userName = principal.getName();
+		System.out.println(" INSIDE USER CONTROLL => DASHBOARD METHOD USER NAME : " + userName);
+		//get the userName(email)
+		
+		User user = userRepository.GetUserByUserName(userName);
+		System.out.println("INSIDE USER CONTROLL => DASHBOARD METHOD USER OBJECT : "+ user);
+		
+		model.addAttribute("user", user);
 		return "normal/user_dashboard";
 
 	}
