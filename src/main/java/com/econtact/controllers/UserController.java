240d570134a1fd04feeeb26210c6942dc2eb0fd0
sushilv3 <sushilv3rma@gmail.com>
2021-03-08 23:08:34 +0530

    added success and error message into process-contact handler

--- src/main/java/com/econtact/controllers/UserController.java
+++ src/main/java/com/econtact/controllers/UserController.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -20,94 +22,101 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.econtact.entities.Contact;
 import com.econtact.entities.User;
+import com.econtact.helper.Message;
 import com.econtact.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	//execute before every handler call "method for adding common data to response"
+
+	// execute before every handler call "method for adding common data to response"
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
-		
+
 		String userName = principal.getName();
 		System.out.println(" INSIDE USER CONTROLL => DASHBOARD METHOD USER NAME : " + userName);
-		//get the userName(email)
-		
+		// get the userName(email)
+
 		User user = userRepository.GetUserByUserName(userName);
-		System.out.println("INSIDE USER CONTROLL => DASHBOARD METHOD USER OBJECT : "+ user);
-		
+		System.out.println("INSIDE USER CONTROLL => DASHBOARD METHOD USER OBJECT : " + user);
+
 		model.addAttribute("user", user);
 	}
-	
-	//dashboard home
+
+	// dashboard home
 	@RequestMapping("/index")
-	public String dashBoard(Model model,Principal principal) {
-		model.addAttribute("title","User Dashboard");
-		
+	public String dashBoard(Model model, Principal principal) {
+		model.addAttribute("title", "User Dashboard");
+
 		return "normal/user_dashboard";
 
 	}
-	//open add form handler
+
+	// open add form handler
 	@GetMapping("add-contact")
 	public String addContactForm(Model model) {
-		model.addAttribute("title","Add Contact");
+		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact", new Contact());
-		
+
 		return "normal/add_contact_form";
 	}
-	
-	//proessing add contact form
+
+	// proessing add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact,@RequestParam("profileImage")MultipartFile file, Principal principal) {
-		
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+			Principal principal, HttpSession session) {
+
 		try {
-		String userName = principal.getName();
-		User user = this.userRepository.GetUserByUserName(userName);
-		
-		contact.setUser(user); 
-		
-		//processing and uploading file...
-		
-		if(file.isEmpty()) {
-			//if the file is empty then try our message
-			System.out.println("file is empty");
+			String userName = principal.getName();
+			User user = this.userRepository.GetUserByUserName(userName);
+
+			contact.setUser(user);
+
+			// processing and uploading file...
+
+			if (file.isEmpty()) {
+				// if the file is empty then try our message
+				System.out.println("file is empty");
+
+			} else {
+				// upload file to folder and update the name to contact
+				contact.setImgUrl(file.getOriginalFilename());
+
+				File saveFile = new ClassPathResource("static/img").getFile();
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				System.out.println("Image is Uploaded" + contact.getImgUrl());
+
+			}
+
+			user.getContacts().add(contact);
+
+			this.userRepository.save(user);
+
+			System.out.println("CONTACT OBJECT : " + contact);
+
+			System.out.println("Added to data Base");
+			System.out.println("User ::::: " + user);
 			
-		}else {
-			//upload file to folder and update the name to contact 
-			contact.setImgUrl(file.getOriginalFilename());
+			//message success.....
+			
+			session.setAttribute("message", new Message("Your Contact is added !! Add more..", "success"));
 			
 			
-		File saveFile =	new ClassPathResource("static/img").getFile();
-		Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
-		
-		Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-		
-		System.out.println("Image is Uploaded"+contact.getImgUrl());
+		} catch (Exception e) {
+			System.out.println("Error " + e.getMessage());
+			e.printStackTrace();
+			//message error
+			session.setAttribute("message", new Message("Something went wrong .... try again", "danger"));
 			
 		}
-		
-		user.getContacts().add(contact);
-		
-		
-		
-		this.userRepository.save(user);
-		
-		
-		System.out.println("CONTACT OBJECT : "+ contact);
-		
-		System.out.println("Added to data Base");
-		System.out.println("User ::::: "+user);
-		}
-		catch(Exception e) {
-		System.out.println("Error "+ e.getMessage());
-		e.printStackTrace();
-		}
-		
+
 		return "normal/add_contact_form";
 	}
-	
+
 }
