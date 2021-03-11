55e9bdfa08674d01dc5ff3863798c3e6ef0b7ed0
sushilv3 <sushilv3rma@gmail.com>
2021-03-11 23:13:42 +0530

    added pagination

--- src/main/java/com/econtact/repositories/ContactRepository.java
+++ src/main/java/com/econtact/repositories/ContactRepository.java
@@ -2,6 +2,8 @@ package com.econtact.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,9 @@ public interface ContactRepository extends JpaRepository<Contact, Integer>{
 	
 	//pagination....
 	@Query("from Contact as c where c.user.id = :userId")
-	public List<Contact> findContactByUser(@Param("userId")int userId);
+	
+	//currentPage = page
+	//Contact per apge = 2
+	public Page<Contact> findContactByUser(@Param("userId")int userId, Pageable pageable);
 
 }
