b8495e49da3f3abd5d790b767323525e16cd1c4d
sushilv3 <sushilv3rma@gmail.com>
2021-03-12 13:11:58 +0530

    added pagination functionality

--- src/main/java/com/econtact/controllers/UserController.java
+++ src/main/java/com/econtact/controllers/UserController.java
@@ -12,10 +12,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -126,15 +130,25 @@ public class UserController {
 	}
 	
 	//show contacts handler
-	@GetMapping("/show-contacts")
-	public String showContacts(Model model,Principal principal) {
+	//per page =2[n]
+	//current page =0[page]
+	@GetMapping("/show-contacts/{page}")
+	public String showContacts(@PathVariable("page") Integer page,Model model,Principal principal) {
 		model.addAttribute("title","Show User Contacts");
 		//sent contacts list
 		String username = principal.getName();
 		User user = this.userRepository.GetUserByUserName(username);
+	
+		Pageable pageable = PageRequest.of(page,2);
 		
-		List<Contact> contacts = this.contactRepository.findContactByUser(user.getId());
+		Page<Contact> contacts = this.contactRepository.findContactByUser(user.getId(),pageable);
+		
 		model.addAttribute("contacts",contacts);
+		
+		model.addAttribute("currentPage",page);
+		
+		model.addAttribute("totalPages",contacts.getTotalPages());
+		
 		System.out.println("@@@@@ called inside show-contact handler ===== Conact List ::::  "+contacts);
 		return "normal/show_contacts";
 	}
