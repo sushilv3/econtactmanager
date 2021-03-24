52ffeb1ad50ae453165be5ede2a3e2d6ee9a4b2e
sushilv3 <sushilv3rma@gmail.com>
2021-03-24 15:03:03 +0530

    added search functionality with ajax request

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,3 +11,43 @@ const toggleSidebar = () => {
         $(".content").css("margin-left", "20%");
     }
 };
+
+const search = () =>{
+
+    //console.log("searching....");
+
+    let query = $("#search-input").val();
+
+    if(query==""){
+
+        $(".search-result").hide();
+
+    }else{
+
+        //console.log(query);
+
+        let url= `http://localhost:8080/search/${query}`;
+
+        fetch(url).then((response)=>{
+
+            return response.json();
+
+        }).then((data)=>{
+            //data
+            //console.log(data);
+            
+            let text=`<div class='list-group'>`
+            
+            data.forEach((contact)=>{
+            	text+= `<a href='/user/contact/${contact.cId}' class='list-group-item list-group-item-action'> ${contact.name}</a>`
+            });
+            
+            text+=`</div>`
+
+				$(".search-result").html(text);
+
+      			$(".search-result").show();
+
+        });  
+    }
+}
