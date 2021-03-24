282c879c22b1fe1418d48ca9877c4bf9fca0fba0
sushilv3 <sushilv3rma@gmail.com>
2021-03-24 14:57:09 +0530

    added @jsonIgnore annotation on manyToOne relation

--- src/main/java/com/econtact/entities/Contact.java
+++ src/main/java/com/econtact/entities/Contact.java
@@ -10,6 +10,8 @@ import javax.persistence.Table;
 
 import org.hibernate.type.StringNVarcharType;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="CONTACT")
 public class Contact {
@@ -26,6 +28,7 @@ public class Contact {
 	private String description;
 	
 	@ManyToOne()
+	@JsonIgnore
 	private User user;
 	
 	public Contact() {
