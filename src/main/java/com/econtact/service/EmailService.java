eac8ca3a883e23c342499d62eaa722becb8c735c
sushilv3 <sushilv3rma@gmail.com>
2021-04-02 23:14:29 +0530

    added service package

---
+++ src/main/java/com/econtact/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.econtact.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	public void sendEmail(String subject, String message, String to) {
+		
+	}
+
+}
