bed0545f282a6edd4f03d940887f926b8689fa6f
sushilv3 <sushilv3rma@gmail.com>
2021-04-04 17:36:36 +0530

    added sent-otp handler

--- src/main/java/com/econtact/controllers/forgotController.java
+++ src/main/java/com/econtact/controllers/forgotController.java
@@ -2,15 +2,23 @@ package com.econtact.controllers;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.econtact.services.EmailService;
+
 @Controller
 public class forgotController {
 
 	Random random =new Random(1000);
+	
+	@Autowired
+	private EmailService emailService;
 	
 	//emmail id form open handler
 	@RequestMapping("/forgot")
@@ -22,7 +30,7 @@ public class forgotController {
 	
 	//send-otp 
 	@PostMapping("/send-otp")
-	public String sentOtp(@RequestParam("email") String email) {
+	public String sentOtp(@RequestParam("email") String email, HttpSession session) {
 		
 		System.out.println("*****Email ***** " + email);
 		
@@ -32,7 +40,27 @@ public class forgotController {
 		System.out.println("^^^^^^ OTP : "+otp);
 		
 		//write code for send otp to email...
-		return "verify_otp";
+		
+		String to = email;
+		
+		String subject = "OTP from econtact manager"; 
+		
+		String body = " Otp = "+otp ;
+		
+		boolean flag = this.emailService.sendEmail(to, body, subject);
+		System.out.println("%%%%% FLAG VALUE  " +flag);
+		
+		if(flag)
+		{
+			session.setAttribute("otp", otp);
+			return "verify_otp";
+		}
+		else {
+			
+			session.setAttribute("message", "Check Your Email Id");
+			return "forgot_email_form";	
+		}
+		
 	}
 	
 }
