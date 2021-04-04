c1f8af97189ab549f4118d9ca8b5ef8630098eee
sushilv3 <sushilv3rma@gmail.com>
2021-04-04 17:38:03 +0530

    created EmailSender Service

--- src/main/java/com/econtact/services/EmailService.java
+++ src/main/java/com/econtact/services/EmailService.java
@@ -1,12 +1,36 @@
-package com.econtact.service;
+package com.econtact.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-	
-	public void sendEmail(String subject, String message, String to) {
-		
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	public boolean sendEmail(String to, String body, String subject) {
+
+		boolean f = false;
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		try {
+			message.setFrom("sushilv3rma@gmail.com");
+			message.setTo(to);
+			message.setText(body);
+			message.setSubject(subject);
+
+			mailSender.send(message);
+
+			System.out.println("Mail Sent....");
+			
+			f=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return f;
 	}
 
 }
